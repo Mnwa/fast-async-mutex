33b97ba5773722d47863ad6b226d1a7458856b47
Mnwa <mihan@panfilov.biz>
2020-09-08 23:43:54 +0300

    Update desc

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,11 @@
+/// `fast_async_mutex` it is a lib which provide asynchronous locking mechanisms, which used spinlock algorithm.
+/// It's maybe very efficient because when mutex tries to acquire data unsuccessfully, these returning control to an async runtime back.
+/// This lib built only on atomics and don't use others std synchronous data structures, which make this lib so fast.
+
 /// The fast async mutex which uses spinlock algorithm with using waker
 pub mod mutex;
+/// The fast async mutex which uses spinlock algorithm with using waker
+/// This realisation dont check an order of mutex acquiring.
 pub mod mutex_unordered;
+/// RwLock realisation which uses spinlock algorithm with using waker
 pub mod rwlock;
